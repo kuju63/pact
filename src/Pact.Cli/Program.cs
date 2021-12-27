1b1d05295d06cbf6d98faad9fffabf6b82898f55
Kurihara Jun <lh182051+src@gmail.com>
2021-12-27 23:06:13 +0900

    :green_heart: add publication

--- src/Pact.Cli/Program.cs
+++ src/Pact.Cli/Program.cs
@@ -6,7 +6,7 @@ namespace Pact.Cli
 {
     public class Program
     {
-        static async Task<int> Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             var rootCommand = new RootCommand
             {
