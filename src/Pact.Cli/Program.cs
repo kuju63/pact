d5c74872588506749b937561f5dbfe8d66b59bf9
Kurihara Jun <lh182051+src@gmail.com>
2021-12-27 22:48:46 +0900

    :sparkles: add root command

--- src/Pact.Cli/Program.cs
+++ src/Pact.Cli/Program.cs
@@ -1,2 +1,24 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+using Pact.Cli.Models;
+
+namespace Pact.Cli
+{
+    public class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Option<string[]>(new[] { "--secrets", "-s" }, ""),
+                new Option<FileInfo>("--secret-file", "").ExistingOnly(),
+                new Option<bool>(new[] { "--dry-run", "-d" }),
+                new Option<string>("--job"),
+                new Option<bool>(new[] { "--list", "-l" }, "List workflows")
+            };
+            rootCommand.Handler = CommandHandler.Create<GlobalOption>((options) => { Console.WriteLine(options.ToString()); });
+
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
